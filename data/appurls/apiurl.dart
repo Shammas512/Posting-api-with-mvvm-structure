fdd74bbb3a706c2f83936452954f6910670e459c
Shammas512 <shammasasif7@gmail.com>
2026-09-06 00:36:34 +0500

    New routes and things

--- data/appurls/apiurl.dart
+++ data/appurls/apiurl.dart
@@ -1,4 +1,6 @@
 class Apiurl {
   static const String baseurl = 'https://reqres.in';
   static const String loginurl = "$baseurl/api/register";
+
+  static const String userlisturl = "https://webhook.site/9ef6f12c-0690-431c-8cda-a505c972429d";
 }
---
+++ repositries/home_repo.dart
@@ -0,0 +1,15 @@
+import 'package:code_x/working/data/appurls/apiurl.dart';
+import 'package:code_x/working/data/network/network_api.dart';
+import 'package:flutter/foundation.dart';
+
+class HomeRepo {
+  final _apiservice = NetworkApiService();
+
+  Future<dynamic> getApi() async {
+    final response = await _apiservice.getApi(Apiurl.userlisturl);
+    if (kDebugMode) {
+      print(response);
+    }
+    return response;
+  }
+}
